4698a85292b7a6c1ae0e4ab26c58cf5dbdb938cc
Rafael Mejia <rafaeldj4@gmail.com>
2024-03-06 21:04:56 -0400

    scrips y archivo de observaciones

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
   pageLoadTimeout: 15000,
-  video:true,
+  video:false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
